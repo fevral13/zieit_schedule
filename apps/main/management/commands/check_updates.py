6ffadcac56ddb01261adb076933b2f6dfab6d136
Serj Zavadsky <fevral13@gmail.com>
2013-12-16 23:21:21 +0200

    check_updates command fixed

--- apps/main/management/commands/check_updates.py
+++ apps/main/management/commands/check_updates.py
@@ -2,7 +2,7 @@
 import urllib2
 
 from django.conf import settings
-from django.core.mail import send_mail
+from django.core.mail import send_mail, EmailMessage
 from django.core.management import BaseCommand
 from django.template.loader import render_to_string
 
@@ -22,9 +22,8 @@ class Command(BaseCommand):
 
             last_modified = response.headers.dict['last-modified']
             content_length = response.headers.dict['content-length']
-
             initial_check = None in (schedule.last_modified, schedule.content_length)
-            modified = unicode(schedule.last_modified) != unicode(last_modified) or unicode(schedule.content_length) != unicode(content_length)
+            modified = unicode(schedule.content_length) != unicode(content_length)
 
             if initial_check or modified:
                 schedule.last_modified = last_modified
@@ -32,11 +31,9 @@ class Command(BaseCommand):
                 schedule.save()
 
                 if modified:
-
+                    file_contents = urllib2.urlopen(schedule.url).read()
                     for subscriber in schedule.subscriber_set.all():
-                        email = subscriber.user.email
                         body = render_to_string('emails/notification.html', {'schedule': schedule})
-                        send_mail(u'Расписание изменилось', body, from_email=settings.DEFAULT_FROM_EMAIL, recipient_list=[email, ], fail_silently=False)
-
-                    print('%s updated' % schedule.name)
-                    print(schedule.last_modified, last_modified, schedule.content_length, content_length)
+                        email = EmailMessage(u'Расписание изменилось', body, settings.DEFAULT_FROM_EMAIL, [subscriber.user.email])
+                        email.attach(schedule.url.split('/')[-1], file_contents, response.headers.dict['content-type'])
+                        email.send()
